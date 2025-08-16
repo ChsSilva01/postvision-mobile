376f8776f8d7de951aa59fb22642a3f995c037a9
carloshenrique <chssilva1419@gmail.com>
2025-08-15 21:31:38 -0300

    0.13

--- app/src/main/java/com/example/postvision/MainActivity.kt
+++ app/src/main/java/com/example/postvision/MainActivity.kt
@@ -10,8 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.splash_scren)) { v, insets ->
+        setContentView(R.layout.login_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login_screen)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
